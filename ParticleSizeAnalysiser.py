0b4d3e97bdba1c6f25a4d68a7db7e794ccc45166
王炳诚 <wangbingcheng@wangbingchengdeMacBook-Pro.local>
2022-03-29 14:58:27 +0800

    update the algorithm

--- ParticleSizeAnalysiser.py
+++ ParticleSizeAnalysiser.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
+from ctypes import sizeof
 import cv2
 import matplotlib
+import numpy as np
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import sys
 from matplotlib.ticker import FuncFormatter
-if sys.argv[1] == None:
-    path = 'test.jpg'
+
+def to_percent(temp, position):
+    return '%1.0f'%(100*temp) + '%'
+
+temp = len(sys.argv)
+if len(sys.argv) < 2:
+    path = 'c40.jpg'
 else:
     path = sys.argv[1]
 plt.rcParams['font.family'] = ['Times New Roman']
 plt.rcParams.update({'font.size': 14})
+
 pic = cv2.imread(path, 0)
 ret, bw = cv2.threshold(pic, 50, 255, cv2.THRESH_BINARY)
 contours, hierarchy = cv2.findContours(bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -20,25 +28,36 @@ for item in contours:
     if temp<5000 and temp > 0:
         area_list.append(temp ** 0.5)
 area_list.sort()
-th = int(area_list[-1]-area_list[0])/3
-low = []
-mid = []
-high = []
+
+point_num = 12
+th = int(area_list[-1]-area_list[0])/point_num
+temp_volume_list = [[] for i in range(point_num)]
+temp_deviation_list = []
 for item in area_list:
-    if item < th:
-        low.append(item**3)
-    elif item >= th and item < 2 * th:
-        mid.append(item**3)
-    else:
-        high.append(item**3)
-total = sum(low) + sum(mid) + sum(high)
-y = [sum(low)/total, sum(mid)/total, sum(high)/total]
-rad = ['Low rad', 'Mid rad', 'High rad']
-print(y)
- 
-plt.bar(rad, y, alpha=0.5, width=0.5, label='Partical Size', lw=3)
-plt.legend()
-def to_percent(temp, position):
-    return '%1.0f'%(100*temp) + '%'
+    for i in range(point_num):
+        if (area_list[0]+th*i)<item and (area_list[0]+th*(i+1))>=item:
+            temp_volume_list[i].append(item**3)
+    if (area_list[0]+th*3)<item and (area_list[0]+th*7)>=item:
+        temp_deviation_list.append(item)
+
+
+sum_volume_list = []
+for i in range(point_num):
+    sum_volume_list.append(sum(temp_volume_list[i]))
+temp_sum = sum(sum_volume_list)
+for i in range(point_num):
+    sum_volume_list[i] /= temp_sum
+grade = sum(sum_volume_list[4:8])
+standard_deviation = np.std(np.array(temp_deviation_list))
+print(grade,standard_deviation)
+
+plt.plot(sum_volume_list)
+plt.ylabel("Volume Ratio %")
+plt.xlabel("Relative Volume")
 plt.gca().yaxis.set_major_formatter(FuncFormatter(to_percent))
 plt.show()
+
+
+
+# plt.bar(rad, y, alpha=0.5, width=0.5, label='Partical Size', lw=3)
+# plt.legend()
--- README.md
+++ README.md
@@ -1,19 +1,29 @@
 # ParticleSizeAnalysiser
 
 
-The particle size distribution of coffee powder can directly affect the flavor of coffee, but the average family will not buy a professional analyzer. This program will give you an idea of the size distribution of the coffee powder coming out of your grinder. This program won't give you specific radius but a rough distribution, like this figure which is belongs to C40.
+The particle size distribution of coffee powder can directly affect the flavor of coffee, but the average family will not buy a professional analyzer. This program will give you an idea of the size distribution of the coffee powder coming out of your grinder. 
 
-## environment
+## Environment
 1.python3
 
 2.opencv4
 
 3.matplotlib
 
-## usage
+## Usage
 ```
-python ParticleSizeAnalysiser.py ./test.jpg
+python ParticleSizeAnalysiser.py ./c40.jpg
 ```
-The *test.jpg* is just an example picture, you can replace it with your own picture.
+The *c40.jpg* is just an example picture, you can replace it with your own picture.
 
-your picture should be like *test.jpg*, whose background is white and coffee powder doesn't overlap.
+your picture should be like *c40.jpg*, whose background is white and coffee powder doesn't overlap.
+
+## Result
+
+The first output is the grade, the higher grade means more powder in similar volume.
+
+The second output is the standard deviation of median volume, the smaller standard deviation means the median volume more average.
+
+The graph shows volume proportion of powder particles with different relative radii.
+
+Here we test lagom mini, C40, we can see C40 has a better result!
